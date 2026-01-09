cc4f00026aacce8d953140cff51e320d8c6f16cd
sashank <sashanksai1064@gmail.com>
2026-01-09 14:06:55 -0800

    VPN & ALB created

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -2,10 +2,29 @@ resource "aws_ssm_parameter" "frontend_sg_id" {
   name  = "/${var.project}/${var.environment}/frontend-sg-id"
   type  = "String"
   value = module.frontend.sg_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "bastion_sg_id" {
   name  = "/${var.project}/${var.environment}/bastion-sg-id"
   type  = "String"
   value = module.bastion.sg_id
+  overwrite = true
+}
+
+
+resource "aws_ssm_parameter" "backend_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/backend-alb-sg-id"
+  type = "String"
+  value = module.backend_alb.sg_id
+  overwrite = true  
+  
+}
+
+resource "aws_ssm_parameter" "vpn_sg_id" {
+  name = "/${var.project}/${var.environment}/vpn-sg-id"
+  type = "String"
+  value = module.vpn.sg_id
+  overwrite = true
+  
 }
---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,13 @@
+variable "project" {
+  description = "Project name"
+  type        = string
+  default = "roboshop"
+  
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default = "dev"
+  
+}

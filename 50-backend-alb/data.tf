7800be5c1543bf18a0491fefe89eb50bf90e33b0
sashank <sashanksai1064@gmail.com>
2026-01-14 23:29:22 -0800

    terraform

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -24,14 +24,14 @@ data "aws_ami" "joindevops" {
 
 
 data "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project}/${var.environment}/vpc-id"
+  name = "/${var.project}/${var.environment}/vpc_id"
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project}/${var.environment}/private-subnet-ids"
+  name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
 data "aws_ssm_parameter" "backend_alb_sg_id" {
-  name = "/${var.project}/${var.environment}/backend-alb-sg-id" 
+  name = "/${var.project}/${var.environment}/backend_alb_sg-id" 
   
 }

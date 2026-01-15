8c4d4398fa0b10cc33eda3dc023c6ff52075a06b
sashank <sashanksai1064@gmail.com>
2026-01-14 23:30:46 -0800

    terraform

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -32,6 +32,6 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 }
 
 data "aws_ssm_parameter" "backend_alb_sg_id" {
-  name = "/${var.project}/${var.environment}/backend_alb_sg-id" 
+  name = "/${var.project}/${var.environment}/backend_alb_sg_id" 
   
 }

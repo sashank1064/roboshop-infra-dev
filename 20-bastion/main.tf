30ed1ff20387f94e9c9b75ad1ccd0f357776149a
sashank <sashanksai1064@gmail.com>
2026-01-19 21:08:32 -0800

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,7 +3,13 @@ resource "aws_instance" "bastion" {
     instance_type          = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
-
+    
+    # To increase the disk space in terraform for the instance to run without showing errors 
+    # we need to mount this in the remote server, if not it does not work
+    root_block_device {
+      volume_size = 50
+      volume_type = "gp3"   # or "gp2", depending on your preference 
+    }
 
     tags = merge(
         local.common_tags, 
